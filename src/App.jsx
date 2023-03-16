9f95b875053813a5598e3038cc2a064b32f5048d
Tarik Sohan <tarikjsohan@gmail.com>
2023-03-16 14:49:05 +0600

    wikitv

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import MainLayout from './components/MainLayout';
+import Show from './components/shows/Show';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { GlobalTheme } from './theme';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
+            <Route path="/show/:showId" element={<Show />} />
+            <Route path="*" element={<div>No Route found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
